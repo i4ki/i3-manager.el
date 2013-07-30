1f5995b799e8d540b1647a091d509ccd07a15ce6
Tiago Natel de Moura <tiago4orion@gmail.com>
2013-07-30 04:06:50 -0300

    Code modularized and implementations of windows related functions

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
 i3-manager.el
 =============
 
-Draft of a i3 manager for EMACS
+*Note: Emacs 24+ required.*
 
-Install:
+This project is a sketch of what may one day become a useful i3 manager
+for emacs.
+
+If you don't know wtf is i3, please follow the link below:
+
+- http://i3wm.org/
+
+The main goal is:
+
+* Create named workspaces and provide tools to moving around them.
+  - 'i3-manager-new-workspace
+  - 'i3-manager-move-to-workspace
+  - 'i3-manager-rename-workspace
+* Find windows and jump to them
+  - 'i3-manager-find-window
+* Exec commands on specific workspaces and track them for future jumps
+  - TODO
+* Create a graphical tree representation of current i3 layout tree
+  - TODO
+* Learn Elisp =)
+
+
+Dependencies
+=============
+
+* i3-emacs: https://github.com/vava/i3-emacs/
+
+Install
+=======
 
 copy i3-manager.el to your emacs load path
 
@@ -11,13 +39,15 @@ and insert the line below to your .emacs or .emacs.d/init.el file:
 
 (require 'i3-manager)
 
-;; set the keybinding to workspace move
+;; set the keybinding you want *Modify as you want*
 (global-set-key (kbd "C-c C-3") 'i3-manager-move-to-workspace)
 (global-set-key (kbd "C-c C-n") 'i3-manager-new-workspace)
+(global-set-key (kbd "C-c C-w") 'i3-manager-find-window)
 
-TODO:
-* View i3 container tree in a 'tree-widget
-* Implement 'i3-manager-move-to-window
-* Implement 'i3-manager-rename-workspace
-* Implement 'i3-manager-execute-on-workspace
-* Others ...
+Contact
+=======
+
+For comments, suggestions or help contact me at:
+     natel (at) secplus.com.br
+
+*Long Live Emacs !*
--- i3-manager.el
+++ i3-manager.el
@@ -1,7 +1,7 @@
 ;; i3-manager
 ;; Author: Tiago Natel de Moura <natel@secplus.com.br>
 ;;
-;; Trying to improve the i3 interface for emacs-hackers
+;; Trying to improve the i3 control for emacs-hackers
 ;;
 
 (require 'cl)
@@ -13,6 +13,9 @@
 (defvar i3-manager-buffer-name "*i3-manager*"
   "Name of the i3-manager buffer")
 
+
+;; Helper functions
+
 (defun print-wspace (wspaces index)
   "Print workspace by INDEX in list of workspace WSPACES"
   (if (and (>= index 0) (< index (length wspaces)))
@@ -20,14 +23,6 @@
              (output (i3-alist-value 'output workspace))
              (name (i3-alist-value 'name workspace)))
         (message "name: %s, output: %s" name output))
-    nil))
-
-(defun i3-manager-get-wspace (wspaces index)
-  "Get workspace by INDEX in list of workspaces WSPACES.
-Returns `nil` if not found"
-  (if (and (>= index 0)
-           (< index (length wspaces)))
-      (elt wspaces index)
     nil))
 
 (defun print-windows (windows)
@@ -38,12 +33,84 @@ Returns `nil` if not found"
     (if next
         (print-windows next))))
 
-;;;###autoload
-(defun i3-manager ()
-  "Start i3-mode buffer"
-  (interactive)
-  (switch-to-buffer i3-manager-buffer-name)
-  (i3-manager-tree-widget))
+(defun i3-manager-get-wspace (wspaces index)
+  "Get workspace by INDEX in list of workspaces WSPACES.
+Returns `nil` if not found"
+  (if (and (>= index 0)
+           (< index (length wspaces)))
+      (elt wspaces index)
+    nil))
+
+(defun i3-manager-get-root-tree ()
+  "Get i3 root tree.
+Returns a associated list (alist) of cons"
+  (i3-get-tree-layout))
+
+(defun i3-manager-get-output-tree (root)
+  "Get i3 vector of active and inactive outputs of i3 ROOT tree."
+  (i3-alist-value 'nodes root))
+
+(defun i3-manager-get-workspaces ()
+  "Get the workspaces tree. Returns a vector of alist's"
+  (setq workspaces '())
+  (let* ((root-tree (i3-manager-get-root-tree))
+         (output-vector (i3-manager-get-output-tree root-tree))
+         (counter 0))
+    (while (< counter (length output-vector))
+      (let* ((output (elt output-vector counter))
+             (output-name (i3-alist-value 'name output)))
+        (if (not (string-match "^\_\_" output-name))
+            ;; i3 internal names starts with __, we avoid that
+            (let ((content-areas (i3-alist-value 'nodes output))
+                  (area-counter 0))
+              ;; content-areas are almost always `topdock`, `bottomdock`
+              ;; or `content`. The last one holds workspaces.
+              (while (< area-counter (length content-areas))
+                (let ((content-area (elt content-areas area-counter)))
+                  (if (string-equal (i3-alist-value 'name content-area)
+                                    "content")
+                      (setq workspaces (i3-alist-value 'nodes
+                                                       content-area))))
+                (setq area-counter (1+ area-counter))))))
+      (setq counter (1+ counter))))
+  workspaces)
+
+(defun i3-manager-get-windows ()
+  "Get i3 windows in a vector of alist containing window-name and
+workspace-name.
+Example result:
+\\[((name . \"emacs\") (\"workspace\" . \"1:dev\"))\\]"
+  (setq window-vector nil)
+  (let* ((workspace-tree (i3-manager-get-workspaces))
+         (wspace-counter 0))
+    (while (< wspace-counter (length workspace-tree))
+      (let* ((workspace (elt workspace-tree wspace-counter))
+             (windows (i3-alist-value 'nodes workspace))
+             (win-counter 0))
+        (while (< win-counter (length windows))
+          (let* ((window (elt windows win-counter))
+                 (window-name (i3-alist-value 'name window))
+                 (window-workspace (i3-alist-value 'name workspace))
+                 (obj (vector `((name . ,window-name)
+                                (workspace . ,window-workspace)))))
+            (if (vectorp window-vector)
+                (setq window-vector (vconcat window-vector obj))
+              (setq window-vector obj)))
+          (setq win-counter (1+ win-counter))))
+      (setq wspace-counter (1+ wspace-counter))))
+  window-vector)
+
+(defun i3-manager-get-windows-names (windows)
+  "Get a list of window's title of WINDOWS.
+Use `i3-manager-get-windows` to get the list of WINDOWS objects"
+  (setq window-titles '())
+  (setq counter 0)
+  (while (< counter (length windows))
+    (let* ((window (elt windows counter))
+           (window-name (i3-alist-value 'name window)))
+      (setq window-titles (append window-titles (list window-name))))
+    (setq counter (1+ counter)))
+  window-titles)
 
 (defun i3-manager-get-wspace-names ()
   (setq wspace-names (list))
@@ -66,9 +133,25 @@ Returns `nil` if not found"
                                  (i3-manager-get-wspace-names))))
   (if wspace-name
       (setq res (i3-command 0 (format "workspace %s"
-                               wspace-name))))
+                                      wspace-name))))
   (if res
       (princ res))
+  res)
+
+(defun i3-manager-find-window (&optional window-name)
+  (interactive)
+  (setq res nil)
+  (setq windows (i3-manager-get-windows))
+  (if (not window-name)
+      (setq window-name
+            (ido-completing-read
+             "Choose a window: "
+             (i3-manager-get-windows-names windows))))
+  (if window-name
+      (let* ((wspace (i3-manager-get-wspace-of-window windows
+                                                      window-name)))
+        (if wspace
+            (setq res (i3-manager-move-to-workspace wspace)))))
   res)
 
 (defun i3-manager-new-workspace (&optional wspace-name)
@@ -86,58 +169,64 @@ Returns `nil` if not found"
       (princ res))
   res)
 
-(defun i3-manager-get-wspace-of-window (name)
-  (let ((wspaces (i3-get-tree-layout)))))
-
-(defun i3-manager-move-to-window (&optional window-title)
-  (interactive)
-  (setq res nil)
-  (if (not window-title)
-      (setq window-title (ido-completing-read
-                          "Choose window title: "
-                          (i3-manager-get-window-names))))
-  (if window-title
-      (let (((wspace (i3-manager-get-wspace-of-window window-title)))
-            (if wspace
-                (i3-manager-move-to-workspace (i3-alist-value 'name
-                                                              wspace))
-              nil)))))
+(defun i3-manager-get-wspace-of-window (windows name)
+  (setq wspace nil)
+  (setq counter 0)
+  (setq found nil)
+  (while (and (not found)
+              (< counter (length windows)))
+    (let* ((obj (elt windows counter))
+           (window-name (i3-alist-value 'name obj))
+           (wspace-name (i3-alist-value 'workspace obj)))
+      (if (string-equal window-name name)
+          (progn
+            (setq wspace wspace-name)
+            (setq found t))))
+    (setq counter (1+ counter)))
+  wspace)
 
 ;;{{{  Widget tree
 ;; testing ...
 (defun i3-manager-tree-widget (&optional theme)
-         (interactive
-          (list (if current-prefix-arg
-                    (completing-read "Theme name: "
-                                     '(("default" . "default")))
-                  nil)))
-         (kill-all-local-variables)
-         (let ((inhibit-read-only t))
-           (erase-buffer))
-         (let ((all (overlay-lists)))
-           (mapcar #'delete-overlay (car all))
-           (mapcar #'delete-overlay (cdr all)))
-         (tree-widget-set-theme theme)
-         (widget-insert (format "%s. \n\n"
-                                "i3 Manager Tree view"))
-         (widget-create
-          ;; Open this level
-          'tree-widget :open t
-          ;; Use a push button for this code
-          :node '(push-button
-                  :tag "Tree"
-                  :format "%[%t%]\n"
-                  :notify
-                  (lambda (&rest ignore)
-                    (message "This is the root node")))
-          ;; Add subtrees (their nodes default to items)
-          '(tree-widget :tag "1:main"
-                        (tree-widget :tag "vertical"
-                                     (item :tag "i4k@secplus ~")
-                                     (item :tag "root@secplus ~")))
-          '(tree-widget :tag "2:www"
-                        (tree-widget :tag "horizontal"
-                                     (item :tag "Google Chrome")
-                                     (item :tag "Firefox")))))
+  (interactive
+   (list (if current-prefix-arg
+             (completing-read "Theme name: "
+                              '(("default" . "default")))
+           nil)))
+  (kill-all-local-variables)
+  (let ((inhibit-read-only t))
+    (erase-buffer))
+  (let ((all (overlay-lists)))
+    (mapcar #'delete-overlay (car all))
+    (mapcar #'delete-overlay (cdr all)))
+  (tree-widget-set-theme theme)
+  (widget-insert (format "%s. \n\n"
+                         "i3 Manager Tree view"))
+  (widget-create
+   ;; Open this level
+   'tree-widget :open t
+   ;; Use a push button for this code
+   :node '(push-button
+           :tag "Tree"
+           :format "%[%t%]\n"
+           :notify
+           (lambda (&rest ignore)
+             (message "This is the root node")))
+   ;; Add subtrees (their nodes default to items)
+   '(tree-widget :tag "1:main"
+                 (tree-widget :tag "vertical"
+                              (item :tag "i4k@secplus ~")
+                              (item :tag "root@secplus ~")))
+   '(tree-widget :tag "2:www"
+                 (tree-widget :tag "horizontal"
+                              (item :tag "Google Chrome")
+                              (item :tag "Firefox")))))
+
+;;;###autoload
+(defun i3-manager ()
+  "Start i3-mode buffer"
+  (interactive)
+  (switch-to-buffer i3-manager-buffer-name)
+  (i3-manager-tree-widget))
 
 (provide 'i3-manager)
